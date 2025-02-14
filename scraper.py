0bbf322195723021c1cc6a673607a684c371c018
leeat5 <156141910+leeat5@users.noreply.github.com>
2025-02-14 14:46:49 -0800

    moved into helper functions + using json instead of a shelve to store tokens

--- scraper.py
+++ scraper.py
@@ -1,25 +1,81 @@
 import re
-from urllib.parse import urlparse, urldefrag, urljoin, urlunparse, unquote
+from urllib.parse import urlparse, urldefrag, urljoin, urlunparse
 from bs4 import BeautifulSoup
 from utils import get_logger
 import nltk
-nltk.download('punkt')
-nltk.download('punkt_tab')
-import shelve
-import csv
+## need to force the downlaods punkt and something ?
 from nltk.tokenize import word_tokenize
+import csv
+import json
+import os
 
 token_shelve = "token_shelve"
 logger = get_logger("SCRAPER")
-traps = ["/pdf/","archive.ics.uci.edu","Nanda", "grape.ics.uci.edu/wiki/public/timeline?","version=","action=login","action=download","github.com","ics.uci.edu/events", "isg.ics.uci.edu/events/tag/talks/day", "share=facebook", "share=twitter", ".pdf", ".ps"]
+traps = ["archive.ics.uci.edu", "Nanda", "timeline?", "version=", "action=login", "action=download", "ics.uci.edu/events", "isg.ics.uci.edu/events/tag/talks/day", "share=facebook", "share=twitter", ".pdf", ".ps"]
+
 
 def scraper(url, resp):
+    links = extract_next_links(url, resp)
+    return [link for link in links if is_valid(link)]
+
+
+def extract_next_links(url, resp):
+    # Implementation required.
+    # url: the URL that was used to get the page
+    # resp.url: the actual url of the page
+    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
+    # resp.error: when status is not 200, you can check the error here, if needed.
+    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
+    #         resp.raw_response.url: the url, again
+    #         resp.raw_response.content: the content of the page!
+    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
     links = []
 
+    already_visited = check_if_visited_page(url)
+    if already_visited:
+        return links
+
+    num = is_valid_response(resp)
+    if num == 4:
+        return links
+    elif num == 3:
+        redirected_url = resp.raw_response.url
+        if is_valid(redirected_url):
+            links.append(redirected_url)
+    elif num == 2:
+        try:
+            # parsing html content
+            soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+
+            if not has_sufficient_content(soup):
+                return links
+
+            if has_nofollow_meta(soup):
+                return links
+
+            links = extract_hyperlinks(url, soup)
+            tokenizer(url, soup)
+        except Exception as e:
+            print(f"Error parsing {url}: {e}")
+
+    links = list(set(links))  # removes duplicates
+    return links
+
+def check_if_visited_page(url) -> bool:
+    """
+    Checks if the page has already been visited.
+    returns an bool value based on status.
+    """
     try:
+
+        if not os.path.exists("all_webpage_count.txt"):
+            open("all_webpage_count.txt", "w").close()
+            open("all_webpage_count_no_stopwords.txt", "w").close()
+
         visited_urls = set()
         decoded_url = url.replace("%7E", "~") ## converts %7E to ~ 
         base_url, frag = urldefrag(decoded_url)
+        base_url = base_url.replace("/www.", "/")
 
         try:
             with open("all_webpage_count.txt", "r") as file:
@@ -32,120 +88,87 @@ def scraper(url, resp):
 
         if base_url in visited_urls:
             logger.info(f"Already visited: {url}")
-            return []            
+            return True            
 
     except Exception as e:
         logger.error(f"Error checking visited URLs: {e}")
-        return []
+        return True
 
-    if "ssh://git@github.com" in url:
-        return []
+    return False            
 
-    if resp.status == 200: #TODO: NEED TO ALLOW REDIRECTS! 200-399
-        try: 
-            content = resp.raw_response.content
-            if not content.strip():
-                logger.info(f"No content: {url}")
-                return []
-            
-            content_soup = BeautifulSoup(content, 'html.parser')
+def is_valid_response(resp) -> int:
+    """
+    Checks if the response is valid (status 200-399 and contains content).
+    returns an int 2 - 200, 3 - 300, 4 - 400
+    """
+    if not resp.raw_response:
+        return 4
 
-## Stops the scraper scraping pages of little content (< 100 words)
-            doc_words = (content_soup.get_text(separator=" ")).split()
-            if len(doc_words) < 100:
-                logger.info(f"Not enough text content: {url}")
-                return []
-            tokenizer(url, doc_words)
+    if 200 <= resp.status < 400:
+        if resp.status >= 300:  # Handle redirects
+            return 3
+        if bool(resp.raw_response.content.strip()):  # Ensure content is not empty
+            return 2
+    return 4
 
-### Scraper does not scrape if page contains no-follow meta tags
-            robot = content_soup.find('meta', attrs={'name': 'robots'})
-            if robot and 'nofollow' in robot.get('content', '').lower():
-                logger.info(f"Skipping bc of nofollow meta tag: {url}")
-                return []
 
-            for anchor in content_soup.find_all('a', href=True):
-                link = anchor['href']
+def has_sufficient_content(soup):
+    """
+    Ensures the page has enough textual content to be worth crawling.
+    """
+    doc_words = (soup.get_text(separator=" ")).split()
+    if len(doc_words) < 100:
+        return False
+    return True
 
-## Had some issues with joining relative links compounding
-## Removes copies of segments that might be repeated
-                full_url = urljoin(url, link)
-                parsed_url = urlparse(full_url)
 
-                path_segments = []
-                for seg in parsed_url.path.split('/'):
-                    if seg and (not path_segments or seg != path_segments[-1]):
-                        path_segments.append(seg)
+def has_nofollow_meta(soup):
+    """
+    Checks if the page has a nofollow meta tag
+    """
+    robot = soup.find('meta', attrs={'name': 'robots'})
+    if robot and 'nofollow' in robot.get('content', '').lower():
+        return robot and 'nofollow' in robot.get('content', '').lower()
 
-                normal_path = '/'.join(path_segments)
-                c_url = urlunparse(parsed_url._replace(path=normal_path))
-                decoded_url = c_url.replace("%7E", "~")
 
-                clean_url, frag = urldefrag(decoded_url) ## Removes fragments (from canvas)
-                logger.info(f"FULL URL: {c_url}")
+def extract_hyperlinks(url, soup):
+    """
+    Extracts hyperlinks from the parsed HTML content
+    """
+    links = []
+    # finds all the <a> tags which mean hyperlink and get their href
+    # example1: <a href="https://www.ics.uci.edu/contact-us"></a>
+    # example2: <a href="about-us"></a>
 
-                links.append(clean_url)
+    for link in soup.find_all('a', href=True):
+        raw_link = link['href']  # extracts the link "https://www.ics.uci.edu/contact-us", "about-us"
+        complete_url = urljoin(url, raw_link)  # joins it to the base url - "https://www.ics.uci.edu/about-us"
+        decoded_url = url_decoder(complete_url) # Converts %7E to ~ in urls so that urls that are encoded do not get duplicated
+        clean_url, _ = urldefrag(decoded_url)  # Remove fragments
+        normal_url = normalize_url(clean_url)
+        if normal_url:
+            links.append(normal_url)
+    return links
 
-        except Exception as e:
-            print(f"Error parsing {url}: {e}")
 
-    links = list(set(links))
-    valid = [link for link in links if is_valid(link)]
-    logger.info(f"{len(valid)} valid links from {url}: {valid}")
-    return valid
-    # links = extract_next_links(url, resp)
-    # return [link for link in links if is_valid(link)]
+def normalize_url(url):
+    """
+    Normalizes URLs to remove redundant parts
+    """
+    parsed_url = urlparse(url)
 
-def tokenizer(url, doc_words):
-    stopwords_set = set(["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"])
+    hostname = parsed_url.hostname
+    if hostname and hostname.startswith("www."):
+        hostname = hostname[4:]
 
-    url_words = 0
-    url_words_no_stop_words = 0
-    
-    with shelve.open("token_shelve", writeback=False) as ts:
-        try:
-            token_frequencies = ts.get("token_frequencies", {})
-            token_frequencies_no_stop_words = ts.get("token_frequencies_no_stop_words", {})
+    path_segments = []
+    for seg in parsed_url.path.split('/'):
+        if seg and (not path_segments or seg != path_segments[-1]):
+            path_segments.append(seg)
 
-            for token in doc_words:
-                token = token.lower()
-                url_words += 1
+    normal_path = '/'.join(path_segments)
+    return urlunparse(parsed_url._replace(netloc=hostname,path=normal_path))
 
-                token_frequencies[token] = token_frequencies.get(token, 0) + 1
-
-                if token not in stopwords_set:
-                    url_words_no_stop_words += 1
-                    token_frequencies_no_stop_words[token] = token_frequencies_no_stop_words.get(token, 0) + 1
-
-            ts["token_frequencies"] = token_frequencies
-            ts["token_frequencies_no_stop_words"] = token_frequencies_no_stop_words
-        except KeyboardInterrupt:
-            print(f"Shuting down program through KeyboardInterrupt: {url}, {e}")
-        except Exception as e:
-            print(f"Error in tokenizing: {url}, {e}")
-        # print(f"courses freq: {token_frequencies.get('courses', 0)}")
-    all_webpage_count = "all_webpage_count.txt"
-    with(open(all_webpage_count, "a")) as file:
-        text_to_write = f"{url},{url_words}\n"        
-        file.write(text_to_write)
-
-    all_webpage_count_no_stopwords = "all_webpage_count_no_stopwords.txt"
-    with(open(all_webpage_count_no_stopwords, "a")) as file:
-        text_to_write = f"{url},{url_words_no_stop_words}\n"
-        file.write(text_to_write)
-    ## probably append to a file to keep track and then convert to a csv for our reports? 
-    ## and then sort it there bc thats easy implementation
-
-def extract_next_links(url, resp):
-    # Implementation required.
-    # url: the URL that was used to get the page
-    # resp.url: the actual url of the page
-    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
-    # resp.error: when status is not 200, you can check the error here, if needed.
-    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
-    #         resp.raw_response.url: the url, again
-    #         resp.raw_response.content: the content of the page!
-    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
@@ -155,32 +178,34 @@ def is_valid(url):
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-
+        
+        # Ensures that there is a hostname before examining the URL
         if not parsed.hostname:
-            logger.info(f"No hostname: {url}")
             return False
 
-## Domain needs to be one of these, allows subdomains
-        if not re.match(r"(.*\.)?(ics|cs|informatics|stat)\.uci\.edu", parsed.hostname): ## domain needs to be one of these, allows subdomains
-            logger.info(f"Not in wanted domain: {url} {parsed.hostname}")
+        # Domain needs to be one of these, allows subdomains
+        if not re.match(r"(.*\.)?(ics|cs|informatics|stat)\.uci\.edu",
+                        parsed.hostname):  # domain needs to be one of these, allows subdomains
             return False
-## Domain can't have any "2000-01-03" etc
+
+        # Domain can't have any "2000-01-03" etc
         if re.search(r"\b\d{4}-\d{2}-\d{2}\b", parsed.path):
             logger.info(f"Date in url: {url}")
             return False
-## Removes unwanted tags in the URL, such as calendars or excessive dates
-        if any(keyword in parsed.query.lower() for keyword in ["timeline", "ical=", "outlook-ical=", "tribe-bar-date=", "eventdate=", "calendar-view", "date="]):
-            logger.info(f"Calendar in url: {url}")
+
+        # Removes unwanted tags in the URL, such as calendars or excessive dates
+        if any(keyword in parsed.query.lower() for keyword in
+               ["ical=", "outlook-ical=", "tribe-bar-date=", "eventdate=", "calendar-view", "date="]):
             return False
-        
+
+        #Ensures that the crawler avoids potential traps
         for t in traps:
             if t in parsed.geturl():
-                logger.info(f"Found a trap in: {url}")
                 return False
 
-## Returns the URL if it doesn't end with any of these extension tags
+        # Returns the URL if it doesn't end with any of these extension tags
         return not re.match(
-            r".*\.(css|js|bmp|gif|jpe?g|ico|img|sql|ipynb|war|bam|mpg|ppsx|apk|pps"
+            r".*\.(css|js|bmp|gif|jpe?g|ico|img|sql|ipynb|war|bam|mpg|ppsx"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
             + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
@@ -189,5 +214,74 @@ def is_valid(url):
             + r"|thmx|mso|arff|rtf|jar|csv"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
     except TypeError:
-        print ("TypeError for ", parsed)
+        print("TypeError for ", parsed)
         raise
+def url_decoder(complete_url):
+    decoded_url = complete_url.replace("%7E", "~")
+    return decoded_url
+
+def tokenizer(url, soup):
+    doc_words = (soup.get_text(separator=" ")).split()
+    stopwords_set = set(["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"])
+    token_frequencies = {}
+    token_frequencies_no_stop_words = {}
+
+    url_words = 0
+    url_words_no_stop_words = 0
+    
+    for line in doc_words:
+        tokenized_line = word_tokenize(line)
+        for token in tokenized_line:
+            token = token.lower()
+            url_words += 1
+            if token not in token_frequencies: 
+                token_frequencies[token] = 1
+            else:
+                token_frequencies[token] += 1
+            if token not in stopwords_set:
+                url_words_no_stop_words += 1
+                if token not in token_frequencies_no_stop_words: 
+                    token_frequencies_no_stop_words[token] = 1
+                else:
+                    token_frequencies_no_stop_words[token] += 1
+    try:
+        token_frequencies_json = "token_frequencies.json"
+        token_frequencies_nostop_json = "token_frequencies_nostop.json"
+        with open(token_frequencies_json, "r") as f:
+            old_frequencies = json.load(f)
+            
+        with open(token_frequencies_nostop_json, "r") as f:
+            old_frequencies_nostop = json.load(f)
+
+    except FileNotFoundError:
+        old_frequencies = {}
+        old_frequencies_nostop = {}
+
+    for token, count in token_frequencies.items():
+        if token in old_frequencies:
+            old_frequencies[token] += count
+        else:
+            old_frequencies[token] = count
+
+    for token, count in token_frequencies_no_stop_words.items():
+        if token in old_frequencies_nostop:
+            old_frequencies_nostop[token] += count
+        else:
+            old_frequencies_nostop[token] = count
+
+    with open(token_frequencies_json, "w") as f:
+        json.dump(old_frequencies, f)
+
+    with open(token_frequencies_nostop_json, "w") as f:
+        json.dump(old_frequencies_nostop, f)
+
+
+    all_webpage_count = "all_webpage_count.txt"
+    with open(all_webpage_count, "a") as file:
+        text_to_write = f"{url},{url_words}\n"        
+        file.write(text_to_write)
+
+    all_webpage_count_no_stopwords = "all_webpage_count_no_stopwords.txt"
+    with open(all_webpage_count_no_stopwords, "a") as file:
+        text_to_write = f"{url},{url_words_no_stop_words}\n"
+        file.write(text_to_write)
